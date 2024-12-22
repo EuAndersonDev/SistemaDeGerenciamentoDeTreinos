fc17225a06bb555ba68060243f1ebafffa1c0512
ANDERSON REIS <137122362+EuAndersonDev@users.noreply.github.com>
2024-12-22 14:04:43 -0300

    Delete ApiTreinos directory

--- ApiTreinos/backend/Routes/sessionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const sessionController = require("../Controllers/sessionController");
-
-router.post("/session", sessionController.createSession);
-router.get("/getAllSession", sessionController.getAllSessions);
-router.get("/sessionByWorkout/:treino_id", sessionController.sessionsByWorkout);
-
-module.exports = router;
